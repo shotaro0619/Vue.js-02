233ff730d08e1c1cb4fce9b549c64bed652407a1
shotaro0619 <66shotaro99@gmail.com>
2021-09-26 22:44:43 +0900

    セクション15

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 var app = new Vue({
   el: '#app',
   data: {
-    age: 0
-  },
-  methods: {
-    clear: function () {
-      this.message = ''
-    },
-    clickHandler: function () {
-      alert('Clicked!')
-    }
+    show: false
   }
 })
